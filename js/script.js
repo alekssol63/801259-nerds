3f71c3247e79768d1f70b7363966b66e096b92c9
alekssol63 <aleksvs83@yandex.ru>
2018-07-05 15:13:18 +0600

    Исправил замечания

--- js/script.js
+++ js/script.js
@@ -12,14 +12,7 @@ var isStorageSupport = true;
 var storageUserName = "";
 var storageEmail  = "";
 var storageLetter = "";
-var interactiveMap =  document.querySelector("iframe");
-interactiveMap.classList.add('active');
-/*
-interactiveMap.addEventListener('mouseover',()=>{
-  interactiveMap.classList.add('active')});
-interactiveMap.addEventListener('mouseout',()=>{
-  interactiveMap.classList.remove('active')})
-*/
+
 function saveData() {
   if (isStorageSupport) {
     localStorage.setItem("userName", userName.value);
@@ -29,7 +22,13 @@ function saveData() {
 }
 try {
   storageUserName = localStorage.getItem("userName");
+  if (userName.hasAttribute('required')) {
+    userName.removeAttribute('required')
+  }
   storageEmail = localStorage.getItem("eMail");
+  if (eMail.hasAttribute('required')) {
+    eMail.removeAttribute('required')
+  }
   storageLetter = localStorage.getItem("letter");
 } catch (err) {
   isStorageSupport = false;
@@ -40,44 +39,39 @@ writeButton.addEventListener("click", function (evt) {
   messageForm.classList.add("active");
   if (storageUserName) {
     userName.value = storageUserName;
-    if (storageEmail) {
-      eMail.value = storageEmail;
-      if (storageLetter) {
-        letter.value = storageLetter;
-      } else {
-          letter.focus();
-      }
-    } else {
-        eMail.focus();
-    }
+    eMail.focus();
   } else {
     userName.focus();
   }
+
+  if (storageEmail) {
+    eMail.value = storageEmail;
+    letter.focus();
+  } else {
+    eMail.focus();
+  }
+
+  if (storageLetter) {
+    letter.value = storageLetter;
+  }
+
 });
+
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   messageForm.classList.remove("active");
   messageForm.classList.remove("modal-error");
   saveData();
 });
+
 messageForm.addEventListener("submit", function (evt) {
   if (!userName.value || !eMail.value || !letter.value) {
-    if (!userName.value) {
-      userName.setAttribute('required', '');
-    }
-    if (!eMail.value) {
-      eMail.setAttribute('required', '');
-    }
-    if (!letter.value) {
-      letter.setAttribute('required', '');
-    }
     evt.preventDefault();
     messageForm.classList.remove("modal-error");
     messageForm.offsetWidth = messageForm.offsetWidth;
     messageForm.classList.add("modal-error");
   } else {
-      
-      saveData();
+    saveData();
   }
 });
 window.addEventListener("keydown", function (evt) {
